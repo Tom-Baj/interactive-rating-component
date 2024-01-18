98e16ef681ac444db410d585bd589cfda544d273
Tom-Baj <140359887+Tom-Baj@users.noreply.github.com>
2024-01-18 15:12:03 +0100

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const buttonNumber = document.querySelectorAll(".numbers");
+const numbers = document.querySelectorAll(".numbers");
+const buttonSubmit = document.getElementById("button-submit");
+const containerSubmit = document.querySelector(".container-submit");
+const containerThankYou = document.querySelector(".container-thank-you");
+const ratingValue = document.getElementById("rating-value");
+
+function score() {
+  for (var i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", function (event) {
+      numbers.forEach(function (button) {
+        button.classList.remove("numbersClicked");
+      });
+      event.target.classList.add("numbersClicked");
+      ratingValue.innerHTML = event.target.innerHTML;
+    });
+  }
+}
+
+score();
+
+buttonSubmit.addEventListener("click", function () {
+  containerSubmit.classList.add("hidden");
+  containerThankYou.classList.remove("hidden");
+});
